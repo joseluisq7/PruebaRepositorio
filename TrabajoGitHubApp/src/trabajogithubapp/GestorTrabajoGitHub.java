fd71fe429a55c92c5fd5948122af01c8e5cf0e06
joseluisq7 <quintanillajoseluis3c@gmail.com>
2020-05-18 09:21:10 -0500

    Mi primer Commit

--- TrabajoGitHubApp/src/trabajogithubapp/GestorTrabajoGitHub.java
+++ TrabajoGitHubApp/src/trabajogithubapp/GestorTrabajoGitHub.java
@@ -15,7 +15,7 @@ public class GestorTrabajoGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("HolaMundo");// TODO code application logic here
     }
     
 }
